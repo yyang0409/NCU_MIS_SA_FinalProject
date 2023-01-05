f529ade9f0ca6be44886a2597e214107087e7633
yyang0409 <106501480+yyang0409@users.noreply.github.com>
2023-01-05 20:58:44 +0800

    Add files via upload

--- NCU_MIS_SA/WEB-INF/classes/ncu/sa/demo/app/ProposalHelper.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/sa/demo/app/ProposalHelper.java
@@ -581,4 +581,93 @@ public class ProposalHelper {
 
         return response;
     }
+    
+    public void updateRaisedFunds(int totalprice,int proposal_id) {
+    	/** 紀錄回傳之資料 */
+        //JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        //long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        
+        try {
+            
+            /** SQL指令 */
+            String sql = "Update `missa`.`proposal` SET `raised_funds` = ?  WHERE `proposal_id` = ?";
+            
+            int raised_funds=getRaisedFunds(proposal_id); 
+            raised_funds=totalprice+raised_funds;
+            
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1,raised_funds);
+            pres.setInt(2,proposal_id);
+            /** 執行更新之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
+        
+        
+    }
+    
+    public int getRaisedFunds(int id) {
+        /** 新建一個 Product 物件之 p 變數，用於紀錄每一位查詢回之商品資料 */
+        Proposal p = null;
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+        int raised_funds = 0;
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `missa`.`proposal` WHERE `proposal`.`proposal_id` = ? LIMIT 1";
+            
+            /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, id);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+            
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            while(rs.next()) {
+            	 /** 將 ResultSet 之資料取出 */
+              raised_funds = rs.getInt("raised_funds");
+            }
+            
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+
+        return raised_funds;
+    }
 }
--- NCU_MIS_SA/WEB-INF/classes/ncu/sa/demo/controller/SponsorRecordController.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/sa/demo/controller/SponsorRecordController.java
@@ -10,6 +10,7 @@ import org.json.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 
+import ncu.sa.demo.app.ProposalHelper;
 import ncu.sa.demo.app.SponsorRecord;
 import ncu.sa.demo.app.SponsorRecordHelper;
 import ncu.sa.tools.JsonReader;
@@ -19,7 +20,7 @@ import ncu.sa.tools.JsonReader;
 public class SponsorRecordController extends HttpServlet {
 	
 	private SponsorRecordHelper sh =  SponsorRecordHelper.getHelper();
-   
+	private ProposalHelper ph =  ProposalHelper.getHelper();
     
 	public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
 		    /** 透過 JsonReader 類別將 Request 之 JSON 格式資料解析並取回 */
@@ -27,6 +28,7 @@ public class SponsorRecordController extends HttpServlet {
 	        JSONObject jso = jsr.getObject();
 	
 	        /** 取出經解析到 JSONObject 之 Request 參數 */
+	        int proposal_id = jso.getInt("proposal_id");
 	        int member_id = jso.getInt("member_id");
 	        int proposal_option_id = jso.getInt("proposal_option_id");
 	        int sponsor_payment_type = jso.getInt("sponsor_payment_type");
@@ -35,16 +37,17 @@ public class SponsorRecordController extends HttpServlet {
 	        int total_price = jso.getInt("total_price");
 	
 	        SponsorRecord sr = new SponsorRecord(member_id,proposal_option_id,sponsor_payment_type,sponsor_amount,proposal_unit_price,total_price);
-	        System.out.print(member_id);
-	        System.out.print(proposal_option_id);
-	        System.out.print(sponsor_payment_type);
-	        System.out.print(sponsor_amount);
-	        System.out.print(proposal_unit_price);
-	        System.out.print(total_price);
+	        System.out.println(proposal_id);
+	        System.out.println(member_id);
+	        System.out.println(proposal_option_id);
+	        System.out.println(sponsor_payment_type);
+	        System.out.println(sponsor_amount);
+	        System.out.println(proposal_unit_price);
+	        System.out.println(total_price);
 	
 	        /** 透過 orderHelper 物件的 create() 方法新建一筆訂單至資料庫 */
 	        JSONObject data = sh.create(sr);
-	
+	        ph.updateRaisedFunds(total_price,proposal_id);
 	        /** 新建一個 JSONObject 用於將回傳之資料進行封裝 */
 	        JSONObject resp = new JSONObject();
 	        resp.put("status", "200");
@@ -53,6 +56,9 @@ public class SponsorRecordController extends HttpServlet {
 	
 	        /** 透過 JsonReader 物件回傳到前端（以 JSONObject 方式） */
 	        jsr.response(resp, response);
+	        
+	        
+	        
 		}
 	
 	 public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException,JSONException {
